28f3e0ba81b8a4a617aaf0975983651575649f7c
hmsajjad <hmsajjad89@gmail.com>
2015-05-25 15:00:38 +0500

    typescript prtoject

---
+++ app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sajjad on 25/05/2015.
+ */
+var a = function () {
+    var a = 1;
+    alert(a);
+};
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by sajjad on 25/05/2015.
+ */
+var a = function(){
+    var a = 1;
+    alert(a);
+}
